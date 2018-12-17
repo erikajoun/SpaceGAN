79983b071b374207a9a5396088cf1dd4f13e005a
ejoun6718 <hjoun@ucsd.edu>
2018-12-17 01:58:26 -0800

    First commit

---
+++ discriminator.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+
+# Discriminator takes in image as input and outputs probabilty between 0 and 1 that input is a real image
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.main = nn.Sequential(
+
+            nn.Conv2d(3, 64, 4, 2, 1, bias = False),
+            nn.LeakyReLU(0.2, inplace = True),
+
+            nn.Conv2d(64, 128, 4, 2, 1, bias = False), 
+            nn.BatchNorm2d(128), 
+            nn.LeakyReLU(0.2, inplace = True), 
+
+            nn.Conv2d(128, 256, 4, 2, 1, bias = False), 
+            nn.BatchNorm2d(256), 
+            nn.LeakyReLU(0.2, inplace = True), 
+
+            nn.Conv2d(256, 512, 4, 2, 1, bias = False), 
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace = True), 
+
+            nn.Conv2d(512, 1, 4, 1, 0, bias = False), 
+            nn.Sigmoid() 
+        )
+
+    def forward(self, input):
+        output = self.main(input)
+        return output.view(-1)
